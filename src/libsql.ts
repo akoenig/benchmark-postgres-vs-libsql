8ad682d1e21b3f9195f3d4aaafd6b82e4bfa816d
André König <andre.koenig@openformation.io>
2023-11-07 07:52:30 +0100

    chore: adjusts indent style

--- src/libsql.ts
+++ src/libsql.ts
@@ -13,21 +13,21 @@ const syncUrl = Bun.env.LIBSQL_DATABASE_URL;
 const authToken = Bun.env.LIBSQL_AUTH_TOKEN;
 
 if (!syncUrl) {
-	console.warn("Please define the LIBSQL_DATABASE_URL environment variable.");
+  console.warn("Please define the LIBSQL_DATABASE_URL environment variable.");
 
-	process.exit(1);
+  process.exit(1);
 }
 
 if (!authToken) {
-	console.warn("Please define the LIBSQL_AUTH_TOKEN environment variable.");
+  console.warn("Please define the LIBSQL_AUTH_TOKEN environment variable.");
 
-	process.exit(1);
+  process.exit(1);
 }
 
 const database = createClient({
-	url: `file://${process.cwd()}/libsql.db`,
-	syncUrl,
-	authToken,
+  url: `file://${process.cwd()}/libsql.db`,
+  syncUrl,
+  authToken,
 });
 
 //
@@ -52,13 +52,13 @@ await database.execute(`
 `);
 
 const seed = Array.from({ length: 1000 }).map(() => ({
-	id: `${Bun.nanoseconds()}${Math.random()}`,
-	username: `user${Bun.nanoseconds()}`,
-	profile: {
-		id: `${Bun.nanoseconds()}${Math.random()}`,
-		firstName: `firstName${Bun.nanoseconds()}`,
-		lastName: `lastName${Bun.nanoseconds()}`,
-	},
+  id: `${Bun.nanoseconds()}${Math.random()}`,
+  username: `user${Bun.nanoseconds()}`,
+  profile: {
+    id: `${Bun.nanoseconds()}${Math.random()}`,
+    firstName: `firstName${Bun.nanoseconds()}`,
+    lastName: `lastName${Bun.nanoseconds()}`,
+  },
 }));
 
 //
@@ -67,20 +67,20 @@ const seed = Array.from({ length: 1000 }).map(() => ({
 console.log("About to seed the database ...");
 
 for (const user of seed) {
-	await database.execute({
-		sql: `INSERT INTO "user" VALUES (?, ?)`,
-		args: [user.id, user.username],
-	});
+  await database.execute({
+    sql: `INSERT INTO "user" VALUES (?, ?)`,
+    args: [user.id, user.username],
+  });
 
-	await database.execute({
-		sql: `INSERT INTO "user_profile" VALUES (?, ?, ?, ?)`,
-		args: [
-			user.profile.id,
-			user.id,
-			user.profile.firstName,
-			user.profile.lastName,
-		],
-	});
+  await database.execute({
+    sql: `INSERT INTO "user_profile" VALUES (?, ?, ?, ?)`,
+    args: [
+      user.profile.id,
+      user.id,
+      user.profile.firstName,
+      user.profile.lastName,
+    ],
+  });
 }
 
 console.log("About to sync to local replica.");
@@ -96,17 +96,17 @@ console.log(`About to measure the performance with ${queryCount} queries ...`);
 const start = Bun.nanoseconds();
 
 for (let i = 0; i < queryCount; i++) {
-	await database.execute(
-		"SELECT u.username, p.first_name FROM user u JOIN user_profile as p ON u.id = p.user_id",
-	);
+  await database.execute(
+    "SELECT u.username, p.first_name FROM user u JOIN user_profile as p ON u.id = p.user_id",
+  );
 }
 
 const delta = (Bun.nanoseconds() - start) / 1000;
 
 console.log(
-	`[libSQL] The whole benchmark took ${duration(delta)}, ${duration(
-		delta / queryCount,
-	)} per query`,
+  `[libSQL] The whole benchmark took ${duration(delta)}, ${duration(
+    delta / queryCount,
+  )} per query`,
 );
 
 await database.close();
--- src/postgres.ts
+++ src/postgres.ts
@@ -12,13 +12,13 @@ import { duration } from "./utils";
 const POSTGRES_DATABASE_URL = Bun.env.POSTGRES_DATABASE_URL;
 
 if (!POSTGRES_DATABASE_URL) {
-	console.warn("Please define the POSTGRES_DATABASE_URL environment variable.");
+  console.warn("Please define the POSTGRES_DATABASE_URL environment variable.");
 
-	process.exit(1);
+  process.exit(1);
 }
 
 const sql = postgres(POSTGRES_DATABASE_URL, {
-	ssl: true,
+  ssl: true,
 });
 
 //
@@ -43,13 +43,13 @@ await sql`
 `;
 
 const seed = Array.from({ length: 1000 }).map(() => ({
-	id: `${Bun.nanoseconds()}${Math.random()}`,
-	username: `user${Bun.nanoseconds()}`,
-	profile: {
-		id: `${Bun.nanoseconds()}${Math.random()}`,
-		firstName: `firstName${Bun.nanoseconds()}`,
-		lastName: `lastName${Bun.nanoseconds()}`,
-	},
+  id: `${Bun.nanoseconds()}${Math.random()}`,
+  username: `user${Bun.nanoseconds()}`,
+  profile: {
+    id: `${Bun.nanoseconds()}${Math.random()}`,
+    firstName: `firstName${Bun.nanoseconds()}`,
+    lastName: `lastName${Bun.nanoseconds()}`,
+  },
 }));
 
 //
@@ -58,10 +58,10 @@ const seed = Array.from({ length: 1000 }).map(() => ({
 console.log("About to seed the database ...");
 
 for (const user of seed) {
-	await sql.begin(async (sql) => {
-		await sql`INSERT INTO "user" VALUES (${user.id}, ${user.username});`;
-		await sql`INSERT INTO "user_profile" VALUES (${user.profile.id}, ${user.id}, ${user.profile.firstName}, ${user.profile.lastName});`;
-	});
+  await sql.begin(async (sql) => {
+    await sql`INSERT INTO "user" VALUES (${user.id}, ${user.username});`;
+    await sql`INSERT INTO "user_profile" VALUES (${user.profile.id}, ${user.id}, ${user.profile.firstName}, ${user.profile.lastName});`;
+  });
 }
 
 //
@@ -73,15 +73,15 @@ console.log(`About to measure the performance with ${queryCount} queries ...`);
 const start = Bun.nanoseconds();
 
 for (let i = 0; i < queryCount; i++) {
-	await sql`SELECT u.username, p.first_name FROM "user" u JOIN "user_profile" as p ON u.id = p.user_id`;
+  await sql`SELECT u.username, p.first_name FROM "user" u JOIN "user_profile" as p ON u.id = p.user_id`;
 }
 
 const delta = (Bun.nanoseconds() - start) / 1000;
 
 console.log(
-	`[Postgres] The whole benchmark took ${duration(delta)}, ${duration(
-		delta / queryCount,
-	)} per query`,
+  `[Postgres] The whole benchmark took ${duration(delta)}, ${duration(
+    delta / queryCount,
+  )} per query`,
 );
 
 await sql.end();
